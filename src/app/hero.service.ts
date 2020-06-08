d62765e2e0410a44196ee8f1274018a80cb44e03
nildemarco <nildemarco@gmail.com>
2020-06-07 22:13:08 -0300

    add fuctionality to add a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,6 +58,13 @@ export class HeroService {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
      tap(_ => this.log(`updated Hero id=${hero.id}`)),
      catchError(this.handleError<any>('updateHero')) 
-    )
-  } 
+    );
+  }
+  
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
